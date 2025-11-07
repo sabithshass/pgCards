557a6b26e04c510dd6e4d7740920a5a6ef142ce8
sabithshass <sabithfoxiom@gmail.com>
2025-11-07 18:30:21 +0530

    commit with user import changed

--- src/controller/googleAuthcontroller.js
+++ src/controller/googleAuthcontroller.js
@@ -1,6 +1,6 @@
 const { OAuth2Client } = require("google-auth-library");
 const jwt = require("jsonwebtoken");
-const User = require("../models/User");
+const User = require("../modals/userSchema");
 
 const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
 
