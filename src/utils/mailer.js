8f6ac9b0d0ec6625cf23a57831b8a3b95398ee47
sabithshass <sabithfoxiom@gmail.com>
2026-01-15 23:28:38 +0530

    email changed

--- src/utils/mailer.js
+++ src/utils/mailer.js
@@ -1,34 +1,49 @@
 // utils/mailer.js
-const nodemailer = require('nodemailer');
-const hbs = require('hbs');
-const path = require('path');
+const nodemailer = require("nodemailer");
+const hbs = require("hbs");
+const path = require("path");
 
+// const transporter = nodemailer.createTransport({
+//   // service: process.env.EMAIL_SERVICE || 'gmail',
+//   host: "smtp.hostinger.com",
+//   auth: {
+//     user: process.env.EMAIL_USER,
+//     pass: process.env.EMAIL_PASS,
+//   },
+// });
 
 const transporter = nodemailer.createTransport({
-  service: process.env.EMAIL_SERVICE || 'gmail', 
+  host: 'smtp.hostinger.com',
+  port: 465,
+  secure: true, 
   auth: {
     user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS 
+    pass: process.env.EMAIL_PASS
   }
 });
 
 const sendEmail = async ({ to, subject, templateName, context }) => {
   try {
-    
-    const templatePath = path.join(__dirname, '../views', `${templateName}.hbs`);
-    const html = hbs.compile(require('fs').readFileSync(templatePath, 'utf-8'))(context);
+    const templatePath = path.join(
+      __dirname,
+      "../views",
+      `${templateName}.hbs`
+    );
+    const html = hbs.compile(require("fs").readFileSync(templatePath, "utf-8"))(
+      context
+    );
 
     await transporter.sendMail({
       from: process.env.EMAIL_USER,
       to,
       subject,
-      html
+      html,
     });
 
     console.log(`Email sent to ${to}`);
   } catch (error) {
-    console.error('Email sending error:', error);
-    throw new Error('Email sending failed');
+    console.error("Email sending error:", error);
+    throw new Error("Email sending failed");
   }
 };
 
